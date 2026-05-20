065ea1bba28ff4c9dd99667c247672276c8481cc
Shridhar Naik <shridharnaikmirjan1@gmail.com>
2026-05-20 12:33:22 +0530

    Designed Task-1

---
+++ src/components/design1/design1.jsx
@@ -0,0 +1,27 @@
+import './design1.css'
+
+function Design1() {
+
+  return (
+    <>
+    <div className="container">
+      <div className="navbar"></div>
+      <div className="first-box">
+        <div className="first-left">Ad Space</div>
+        <div className="first-right">Rotator</div>
+      </div>
+      <div className="second-box">
+        <div className="second-left">
+          <div className="small"></div>
+          Featured Child
+        </div>
+        <div className="second-right"></div>
+      </div>
+      <div className="third-box"></div>
+      <div className="fourth-box"></div>
+    </div>
+    </>
+  )
+}
+
+export default Design1
