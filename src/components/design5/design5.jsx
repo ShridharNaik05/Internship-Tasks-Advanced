b6d44fbf3d485c0eed3e8afdbeb128244c3eab4f
Shridhar Naik <shridharnaikmirjan1@gmail.com>
2026-05-21 13:43:53 +0530

    Designed some layout in Task-5

--- src/components/design5/design5.jsx
+++ src/components/design5/design5.jsx
@@ -1,11 +1,39 @@
-import './design4.css'
+import './design5.css'
 import playButton from '../../assets/play-button.png';
 
 function Design5() {
 
   return (
     <>
-      
+      <div className="container">
+      <div className="navbar">
+        <div className="nav-left">LOGO</div>
+        <div className="nav-right">HOME PAGE</div>
+      </div>
+      <div className="first-box">
+        <div className="first-left"><h3>Ad Space</h3></div>
+        <div className="first-right"><h3>Rotator</h3></div>
+      </div>
+      <div className="second-box">
+        <div className="second-left">
+            <div className="second-left1"><h3>Graphic ad for Event</h3></div>Text for Event
+            <div className="second-left2"><h3>Graphic ad for Event</h3></div>Text for Event
+        </div>
+        <div className="second-right"><h3>Calender</h3>
+            
+        </div>
+      </div>
+      <div className="fourth-box">
+        <div className="boxs">
+          <div className="box1"></div>
+        <div className="box2"></div>
+        <div className="box3"></div>
+        <div className="box4"></div>
+        <div className="box5"></div>
+        </div>
+        <h3>Pics + Mini Bios of Children</h3>
+      </div>
+    </div>
     </>
   )
 }
