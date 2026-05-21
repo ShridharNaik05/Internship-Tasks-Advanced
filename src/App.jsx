e788df6867b67abae2e7b2966871be1a2ff88c4e
Shridhar Naik <shridharnaikmirjan1@gmail.com>
2026-05-21 10:55:22 +0530

    Designed Task-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import Design1 from './components/design1/design1'
+// import Design1 from './components/design1/design1'
+import Design2 from './components/design2/design2'
 
 function App() {
   return (
     <>
-      <Design1 />
+      <Design2 />
     </>
   )
 }
