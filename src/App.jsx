0ca657c78464786c1ac72d8c96196bbe924eee0d
Shridhar Naik <shridharnaikmirjan1@gmail.com>
2026-05-21 12:39:55 +0530

    Completed the Task-4 Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 // import Design1 from './components/design1/design1'
 // import Design2 from './components/design2/design2'
-import Design3 from './components/design3/design3'
+// import Design3 from './components/design3/design3'
+import Design4 from './components/design4/design4'
 
 function App() {
   return (
     <>
-    {/* <Design1 />
-    <Design2 /> */}
+    {/* <Design1 /> */}
+
+    {/* <Design2 /> */}
     
-      <Design3 />
+    {/* <Design3 /> */}
+
+    <Design4 />
+
     </>
   )
 }
---
+++ src/components/design4/design4.jsx
@@ -0,0 +1,35 @@
+import './design4.css'
+import playButton from '../../assets/play-button.png';
+
+function Design4() {
+
+  return (
+    <>
+      <div className="container">
+      <div className="navbar">
+        <div className="nav-left">LOGO</div>
+        <div className="nav-right">HOME PAGE</div>
+      </div>
+      <div className="first-box">
+        <div className="first-left"><h3>Ad Space</h3></div>
+        <div className="first-right"><h3>Rotator</h3></div>
+      </div>
+      <div className="second-box">
+        <h3>Donation Info + Transaction</h3>
+      </div>
+      <div className="fourth-box">
+        <div className="boxs">
+          <div className="box1"></div>
+        <div className="box2"></div>
+        <div className="box3"></div>
+        <div className="box4"></div>
+        <div className="box5"></div>
+        </div>
+        <h3>Pics + Mini Bios of Children</h3>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Design4
