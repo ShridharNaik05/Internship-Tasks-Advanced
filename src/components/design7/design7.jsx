c9ab5a8001f5460175f4b123281d0c4b364ad8cd
Shridhar Naik <shridharnaikmirjan1@gmail.com>
2026-05-25 00:16:16 +0530

    Completed Task-7 Design

---
+++ src/components/design7/design7.jsx
@@ -0,0 +1,80 @@
+import './design7.css'
+
+function Design7() {
+
+  return (
+    <>
+      <div className="container">
+      <div className="navbar">
+        <div className="nav-left">LOGO</div>
+        <div className="nav-right">HOME PAGE</div>
+      </div>
+      <div className="first-box">
+        <div className="first-left"><h3>Ad Space</h3></div>
+        <div className="first-right"><h3>Rotator</h3></div>
+      </div>
+            <div className="second-box">
+            <div className="second-top">
+
+                <div className="item">
+                <div className="div"></div>
+                <p>Mini Child Bio</p>
+                </div>
+
+                <div className="item">
+                <div className="div"></div>
+                <p>Mini Child Bio</p>
+                </div>
+
+                <div className="item">
+                <div className="div"></div>
+                <p>Mini Child Bio</p>
+                </div>
+
+                <div className="item">
+                <div className="div"></div>
+                <p>Mini Child Bio</p>
+                </div>
+
+            </div>
+
+            <div className="second-bottom">
+
+                <div className="item">
+                <div className="div"></div>
+                <p>Mini Child Bio</p>
+                </div>
+
+                <div className="item">
+                <div className="div"></div>
+                <p>Mini Child Bio</p>
+                </div>
+
+                <div className="item">
+                <div className="div"></div>
+                <p>Mini Child Bio</p>
+                </div>
+
+                <div className="item">
+                <div className="div"></div>
+                <p>Mini Child Bio</p>
+                </div>
+
+            </div>
+            </div>
+      <div className="fourth-box">
+        <div className="boxs">
+          <div className="box1"></div>
+        <div className="box2"></div>
+        <div className="box3"></div>
+        <div className="box4"></div>
+        <div className="box5"></div>
+        </div>
+        <h3>Pics + Mini Bios of Children</h3>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Design7
